2ea21553ab4696a71b0aca97e27dbaceaa56e94e
uojo <499620917@qq.com>
2016-07-08 19:18:52 +0800

    feat

--- src/entry1.js
+++ src/entry1.js
@@ -1,12 +1,13 @@
 require("./1.css");
 require("./mod_es5");
 
+// ejs模板
 var tplF1 = require("./2.ejs");
 var html1 = tplF1({a1:"world!"});
 console.log( "template.ejs", html1 );
 document.getElementById("j_ct1").innerHTML = html1;
 
-
+// es6模板
 var tplData = {a1:100,a2:200};
 var html2 = (`es6的模板${tplData.a1}、${tplData.a2} \n "追加" 内容`);
 console.log( "template.es6", html2 );
