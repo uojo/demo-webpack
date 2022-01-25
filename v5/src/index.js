72224c6b3bc6f7d3d9c3c63e786e3da55e968df8
uojo <friend1020@163.com>
2022-01-25 17:07:15 +0800

    save

--- v5/src/index.js
+++
@@ -1,9 +0,0 @@
-require('./import_a')
-
-console.log('92.module.hot', module.hot);
-// if (module.hot) {
-//   module.hot.accept('./import_a.js', function () {
-//     console.log('Accepting the updated printMe module!');
-//     printMe();
-//   })
-// }
